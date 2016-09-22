ff91591c6b2876ce10d15709e30ca2b77097f407
hccarlos <chenhanchih@gmail.com>
2016-09-22 11:31:43 -0700

    initial commit ready for deployment

---
+++ server/config/routes.js
@@ -0,0 +1,16 @@
+var friendsController = require('../controllers/friends.js')
+
+console.log("future routes loading");
+
+module.exports = function(app){
+	// app.get('/', function(req, res){
+		// console.log(req.body);
+		// console.log(req.params.test)
+	// });
+	app.get("/friends", friendsController.index); // show all friends
+	app.get("/friends/:id", friendsController.show); // show one friend
+	app.post("/friends", friendsController.create); // create a new friend
+	app.put("/friends/:id", friendsController.update); // update a friend info
+	app.delete("/friends/:id", friendsController.delete); // delete a friend
+}
+// this adds route listeners to friends for 5 of 7 RESTful routes, excluding new and edit
